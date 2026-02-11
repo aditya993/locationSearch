ee29f394ce9dfbd20745ca77f20885859ab4f9b3
aditya993 <adityarao993@gmail.com>
2026-02-11 16:14:01 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import requests
 from geopy.distance import geodesic
+import os
 
 app = Flask(__name__)
 
@@ -59,4 +60,5 @@ def search():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
